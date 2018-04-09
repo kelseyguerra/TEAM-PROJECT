fc55211ebe3bc19aa91c3f87a0d49656b4ddc488
Kayl Eubanks, Mike Chu and Nick Kardish <nick@>
2018-04-09 15:09:07 -0700

    Add lose condition, add button hider and refresh button

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ function moveNpc(enemy) {
     // Border Checks
     if (enemy.xcoordinate === 0) {
       enemy.xcoordinate += 1;
-    } else if (enemy.xcoordinate === 3) {
+    } else if (enemy.xcoordinate === 5) {
       enemy.xcoordinate -= 1;
     // Left
     } else if (coinFlip() === 0) {
@@ -28,7 +28,7 @@ function moveNpc(enemy) {
     // Border Checks
     if (enemy.ycoordinate === 0) {
       enemy.ycoordinate += 1;
-    } else if (enemy.ycoordinate === 3) {
+    } else if (enemy.ycoordinate === 5) {
       enemy.ycoordinate -= 1;
     // Up
     } else if (coinFlip() === 0) {
@@ -40,6 +40,21 @@ function moveNpc(enemy) {
   }
 }
 
+
+function condition(player, toilet, enemy) {
+  if (player.xcoordinate === toilet.xcoordinate && player.ycoordinate === toilet.ycoordinate) {
+    alert("You win, now you get to shit.");
+    $(".navigation").hide();
+    $(".refresh").show();
+  } else if(player.xcoordinate === enemy.xcoordinate && player.ycoordinate === enemy.ycoordinate) {
+    alert("You lose!");
+    $(".navigation").hide();
+    $(".refresh").show();
+  }
+}
+
+// UI Logic
+
 function redraw(objectArray){
   $("td").text("");
   objectArray.forEach(function(element){
@@ -47,20 +62,18 @@ function redraw(objectArray){
   });
 }
 
-function condition(player, toilet) {
-  if (player.xcoordinate === toilet.xcoordinate && player.ycoordinate === toilet.ycoordinate) {
-    alert("You win, now you get to shit.")
-  }
-}
 
 $(document).ready(function(){
   var objectArray = [];
-  var enemy = new GameObject("X", 0, 3);
+  var enemy = new GameObject("X", 0, 5);
   var player = new GameObject("O", 0, 0);
-  var toilet = new GameObject("T", 3, 3);
+  var toilet = new GameObject("T", 5, 5);
   objectArray.push(toilet);
   objectArray.push(player);
   objectArray.push(enemy);
+
+
+
   redraw(objectArray);
   $("button#move-left").click(function(event) {
     event.preventDefault();
@@ -69,16 +82,16 @@ $(document).ready(function(){
     }
     moveNpc(enemy);
     redraw(objectArray);
-    condition(player, toilet);
+    condition(player, toilet, enemy);
   });
   $("button#move-right").click(function(event) {
     event.preventDefault();
-    if (player.xcoordinate < 3) {
+    if (player.xcoordinate < 5) {
       player.xcoordinate = player.xcoordinate + 1;
     }
     moveNpc(enemy);
     redraw(objectArray);
-    condition(player, toilet);
+    condition(player, toilet, enemy);
   });
   $("button#move-up").click(function(event) {
     event.preventDefault();
@@ -87,15 +100,15 @@ $(document).ready(function(){
     }
     moveNpc(enemy);
     redraw(objectArray);
-    condition(player, toilet);
+    condition(player, toilet, enemy);
   });
   $("button#move-down").click(function(event) {
     event.preventDefault();
-    if (player.ycoordinate < 3) {
+    if (player.ycoordinate < 5) {
       player.ycoordinate = player.ycoordinate + 1;
     }
     moveNpc(enemy);
     redraw(objectArray);
-    condition(player, toilet);
+    condition(player, toilet, enemy);
   });
 });
