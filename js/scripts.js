f01be075524823e5580df6115529e319a30288f0
Kayl Eubanks, Mike Chu and Nick Kardish <mike@>
2018-04-09 13:44:46 -0700

    Refactor player movement, add goal object and win condition function

--- js/scripts.js
+++ js/scripts.js
@@ -4,42 +4,57 @@ function GameObject (symbol, xcoordinate, ycoordinate) {
   this.ycoordinate = ycoordinate;
 }
 
-GameObject.prototype.move = function () {
-  var y = "y" + this.ycoordinate;
-  var yTarget = document.getElementsByClassName(y);
-  var xTarget = $(yTarget).find(".x"+this.xcoordinate);
-  $(xTarget).text(this.symbol);
-};
+
+function redraw(objectArray){
+  $("td").text("");
+  objectArray.forEach(function(element){
+    $(".y" + element.ycoordinate + " .x" + element.xcoordinate).text(element.symbol);
+  });
+}
+
+function condition(player, toilet) {
+  if (player.xcoordinate === toilet.xcoordinate && player.ycoordinate === toilet.ycoordinate) {
+    alert("You win, now you get to shit.")
+  }
+}
 
 $(document).ready(function(){
+  var objectArray = [];
   var player = new GameObject("O", 0, 0);
-  player.move();
+  var toilet = new GameObject("T", 3, 3);
+  objectArray.push(toilet);
+  objectArray.push(player);
+  redraw(objectArray);
   $("button#move-left").click(function(event) {
     event.preventDefault();
     if (player.xcoordinate > 0) {
       player.xcoordinate = player.xcoordinate - 1;
     }
-    player.move();
+    redraw(objectArray);
+    condition(player, toilet);
   });
   $("button#move-right").click(function(event) {
     event.preventDefault();
     if (player.xcoordinate < 3) {
       player.xcoordinate = player.xcoordinate + 1;
     }
-    player.move();
+    redraw(objectArray);
+    condition(player, toilet);
   });
   $("button#move-up").click(function(event) {
     event.preventDefault();
     if (player.ycoordinate > 0) {
       player.ycoordinate = player.ycoordinate - 1;
     }
-    player.move();
+    redraw(objectArray);
+    condition(player, toilet);
   });
-  $("button#move-left").click(function(event) {
+  $("button#move-down").click(function(event) {
     event.preventDefault();
     if (player.ycoordinate < 3) {
       player.ycoordinate = player.ycoordinate + 1;
     }
-    player.move();
+    redraw(objectArray);
+    condition(player, toilet);
   });
 });
