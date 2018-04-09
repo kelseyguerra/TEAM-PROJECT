c103999d8e51dde28ee04eb4ddae199513c23676
kelsey guerra and Susan Southall <susan@>
2018-04-09 15:24:19 -0700

    Add horizontal and vertical NPC movement patterns.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,24 @@ var enemy = {
   type: "NPC",
   xCoord: 3,
   yCoord: 3,
+  direction: "up"
+}
+
+function movePattern (type) {
+  if (type === "random") {
+    moveNpcRandom();
+  } else if (type === "horizontal") {
+    moveNPCHorizontal();
+  }else if (type === "vertical") {
+    moveNPCVertical();
+  }
 }
 
 function coinFlip() {
   return Math.floor(Math.random() * 2);
 }
 
-function moveNpc() {
+function moveNpcRandom() {
   // Horizontal Move
   if (coinFlip() === 0) {
     // Border Checks
@@ -42,12 +53,53 @@ function moveNpc() {
   $(".y" + enemy.yCoord + " .x" + enemy.xCoord).text("X");
 }
 
+function moveNPCHorizontal() {
+  if (enemy.direction === "right") {
+    if (enemy.xCoord < 3) {
+      enemy.xCoord += 1;
+    } else {
+      enemy.xCoord -= 1;
+      enemy.direction = "left";
+    }
+  } else {
+    if (enemy.xCoord > 0) {
+      enemy.xCoord -= 1;
+    } else {
+      enemy.xCoord += 1;
+      enemy.direction = "right";
+    }
+  }
+  $("td").text("");
+  $(".y" + enemy.yCoord + " .x" + enemy.xCoord).text("X");
+}
+
+function moveNPCVertical() {
+  if (enemy.direction === "down") {
+    if (enemy.yCoord < 3) {
+      enemy.yCoord += 1;
+    } else {
+      enemy.yCoord -= 1;
+      enemy.direction = "up";
+    }
+  } else {
+    if (enemy.yCoord > 0) {
+      enemy.yCoord -= 1;
+    } else {
+      enemy.yCoord += 1;
+      enemy.direction = "down";
+    }
+  }
+  $("td").text("");
+  $(".y" + enemy.yCoord + " .x" + enemy.xCoord).text("X");
+}
+
 $(document).ready(function() {
+
   // SET NPC POSITION
   $(".y" + enemy.yCoord + " .x" + enemy.xCoord).text("X");
 
   // TRIGGER NPC MOVEMENT
   $("#test-npc").click(function() {
-    moveNpc();
+    movePattern("vertical");
   });
 });
